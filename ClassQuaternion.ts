b2492b358e31b5691a246baa2995c845e3ca13c9
caiden20000 <121005054+caiden20000@users.noreply.github.com>
2023-03-14 03:19:54 -0400

    WIP Quaternions

--- ClassQuaternion.ts
+++ ClassQuaternion.ts
@@ -1,8 +1,27 @@
-
-
 export { Quaternion };
 
 class Quaternion {
-  constructor(public x: number, public y: number, public z: number, public w: number) {}
-  
+  constructor(
+    public x: number,
+    public y: number,
+    public z: number,
+    public w: number
+  ) {
+    this.normalize();
+  }
+
+  normalize() {
+    let magnitude = Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
+    this.x /= magnitude;
+    this.y /= magnitude;
+    this.z /= magnitude;
+  }
+
+  add(quat: Quaternion): Quaternion {
+    return this;
+  }
+
+  multiply(quat: Quaternion): Quaternion {
+    return this;
+  }
 }
